fc2342baccbcff5f48756569691c89fb6fa8bd7b
Christian <cphvb91@gmail.com>
2023-03-22 15:31:23 +0100

    adding toast notifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
+import Navbar from '@/components/Navbar'
 import Providers from '@/components/Providers'
+import { Toaster } from '@/components/ui/Toast'
 import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
 import { cn } from '../lib/utils'
@@ -17,7 +19,8 @@ export default function RootLayout({
     >
       <body className='min-h-screen bg-slate-50 dark:bg-slate-900 antialiased'>
         <Providers>{children}
-        
+          <Toaster position='bottom-right'/>
+        {/* @ts-expect-error Server Component */}
         <Navbar/>
         </Providers>
         {/* Allow for more height on mobile devices */}
