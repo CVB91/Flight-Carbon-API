92533f6127cca12c80c9da69c238e30bbf0cde82
Christian <cphvb91@gmail.com>
2023-03-22 12:55:40 +0100

    Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Providers from '@/components/Providers'
 import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
 import { cn } from '../lib/utils'
@@ -15,7 +16,12 @@ export default function RootLayout({
       className={cn('bg-white text-slate-900 antialiased', inter.className)}
     >
       <body className='min-h-screen bg-slate-50 dark:bg-slate-900 antialiased'>
-        <Providers>{children}</Providers>
+        <Providers>{children}
+        
+        <Navbar/>
+        </Providers>
+        {/* Allow for more height on mobile devices */}
+        <div className='h-40 md:hidden'></div>
       </body>
     </html>
   )
