de9ca5625f58798287aa6113796b8b14dd8d9a94
Christian <cphvb91@gmail.com>
2023-03-24 21:05:25 +0100

    preflight headers

--- src/lib/api-middleware/with-methods.ts
+++ src/lib/api-middleware/with-methods.ts
@@ -3,6 +3,8 @@ import type { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
 export function withMethods(methods: string[], handler: NextApiHandler) {
   return async function (req: NextApiRequest, res: NextApiResponse) {
     
+    // this is what I added to try to make it work
+    
      if (req.method === 'OPTIONS') {
       return res.status(200).json({ body: 'OK' })
      }
