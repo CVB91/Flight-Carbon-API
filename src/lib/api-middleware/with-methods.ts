b406745d1d281316276853009886baba5b5cdc68
Christian <cphvb91@gmail.com>
2023-03-24 20:04:34 +0100

    preflight

--- src/lib/api-middleware/with-methods.ts
+++ src/lib/api-middleware/with-methods.ts
@@ -2,6 +2,9 @@ import type { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
 
 export function withMethods(methods: string[], handler: NextApiHandler) {
   return async function (req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'OPTIONS') {
+      return res.status(200).json({ body: 'OK' })
+    }
     if (!req.method || !methods.includes(req.method)) {
       return res.status(405).end()
     }
